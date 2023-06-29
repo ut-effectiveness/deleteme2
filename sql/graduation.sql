9a3194f95016e7712742ebc162c88e3c2bcc971f
JoyBaule <52751421+JoyBaule@users.noreply.github.com>
2023-06-29 11:09:27 -0600

    R training 6/29

--- sql/graduation.sql
+++ sql/graduation.sql
@@ -1,14 +1,21 @@
-/* Graduation Query  */
-
-      SELECT a.student_id AS headcount,
+/* Graduation Query from Export */
+   SELECT a.student_id AS headcount,
           a.graduation_date,
           a.degree_id AS primary_degree_id,
           CASE WHEN a.degree_id IN ('MMFT', 'MAT', 'MACC', 'MA') THEN 'Masters'
               WHEN a.degree_id IN ('CERT', 'CER1', 'CER0') THEN 'Certificate'
-              WHEN a.degree_id IN ('AA', 'AA', 'AAS', 'AS', 'AB', 'AC', 'APE') THEN 'Associates'
+              WHEN a.degree_id IN ('AA', 'AAS', 'AS', 'AB', 'AC', 'APE') THEN 'Associates'
               WHEN a.degree_id IN ('BA', 'BAS', 'BFA', 'BIS','BM', 'BS', 'BSN') THEN 'Bachelors'
                   ELSE 'ERROR'
                       END "Degree",
+          CASE WHEN a.graduated_term_id IN ('202230', '202240', '202320') THEN '2022-23'
+               WHEN a.graduated_term_id IN ('202130', '202140', '202220') THEN '2021-22'
+               WHEN a.graduated_term_id IN ('202030', '202040', '202120') THEN '2020-21'
+               WHEN a.graduated_term_id IN ('201930', '201940', '202020') THEN '2019-20'
+               WHEN a.graduated_term_id IN ('201830', '201840', '201920') THEN '2018-19'
+               WHEN a.graduated_term_id IN ('201730', '201740', '201820') THEN '2017-18'
+                   ELSE 'ERROR'
+                       END "reporting_year",
           a.level_id,
           --a.graduated_academic_year_code,
           e.term_desc,
@@ -32,4 +39,4 @@ LEFT JOIN export.academic_programs d
 LEFT JOIN export.term e
        ON e.term_id = a.graduated_term_id
     WHERE a.degree_status_code = 'AW'
-      AND EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM graduation_date) <= 5 -- Past 5 years
+      AND EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM graduation_date) <= 6 -- Past 6 years
